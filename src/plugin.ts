6a58934d9ebe023bb0d682c2bf9f64ef059ca596
kamiazya <yuki@kamiazya.tech>
2019-03-08 20:31:59 +0900

    fix plugin for readability

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,6 +1,7 @@
 import { Converter } from 'typedoc/dist/lib/converter';
 import { Component, ConverterComponent } from 'typedoc/dist/lib/converter/components';
 import { Context } from 'typedoc/dist/lib/converter/context';
+import { Comment, CommentTag } from 'typedoc/dist/lib/models/comments';
 import { PageEvent } from 'typedoc/dist/lib/output/events';
 
 /**
@@ -9,8 +10,17 @@ import { PageEvent } from 'typedoc/dist/lib/output/events';
 @Component({ name: 'mermaid' })
 export class MermaidPlugin extends ConverterComponent {
 
-  private static metaMatcher = /<\/body>/;
-  private static script = `
+  /**
+   * Regex literal that matches body closing tag.
+   */
+  private static BODY_CLOSINNG_TAG = /<\/body>/;
+
+  /**
+   * 1. Load mermaid.js liblary.
+   * 2. Initialize mermaid.
+   * 3. Close body tag.
+   */
+  private static CUSTOM_SCRIPTS_AND_BODY_CLOSINNG_TAG = `
     <script
       src="https://unpkg.com/mermaid@7.1.2/dist/mermaid.min.js"
     ></script>
@@ -21,6 +31,79 @@ export class MermaidPlugin extends ConverterComponent {
     </script>
     </body>
   `;
+
+  /**
+   * fillter logic for Comment exist
+   */
+  private static filterComment(comment: undefined | Comment): comment is Comment {
+    return comment !== undefined && comment instanceof Comment;
+  }
+
+  /**
+   * fillter logic for CommentTags exist
+   */
+  private static filterCommentTags(tags: CommentTag[] | undefined): tags is CommentTag[] {
+    return tags !== undefined && !!tags;
+  }
+
+  /**
+   * return turn when tag's paramName is 'mermaid'
+   */
+  private static isMermaidCommentTag(tag: CommentTag): boolean {
+    return tag.paramName === 'mermaid';
+  }
+
+  /**
+   * The first line of text wraps h4.
+   * The other wraps by div classed mermaid.
+   */
+  private static convertCommentTag(tag: CommentTag): void {
+    const texts = tag.text.split('\n');
+    // take first line
+    const title = texts.shift();
+    // the other
+    const mermaid = texts.join('\n');
+    tag.text = `#### ${title} \n\n <div class="mermaid">${mermaid}</div>`;
+  }
+
+  /**
+   * Insert custom script before closing body tag.
+   */
+  private static convertPageContents(contents: string): string {
+    if (this.BODY_CLOSINNG_TAG.test(contents)) {
+      return contents.replace(
+        this.BODY_CLOSINNG_TAG,
+        this.CUSTOM_SCRIPTS_AND_BODY_CLOSINNG_TAG);
+    }
+    return contents;
+  }
+
+  /**
+   * get CommentTags for using `@mermaid` anotation from Context.
+   */
+  private static mermaidTags(context: Context): CommentTag[] {
+    return Object
+      // get reflection from context
+      .values(context.project.reflections)
+
+      // get Comment from Reflection
+      .map(reflection => reflection.comment)
+
+      // filter only comment exist
+      .filter(this.filterComment)
+
+      // get CommentTags from Comment
+      .map(comment => comment.tags)
+
+      // filter only CommentTags exist
+      .filter(this.filterCommentTags)
+
+      // merge all CommentTags
+      .reduce((a, b) => a.concat(b), [])
+
+      // filter tag that paramName is 'mermaid'
+      .filter(this.isMermaidCommentTag);
+  }
 
   /**
    * listen to event on initialisation
@@ -39,22 +122,9 @@ export class MermaidPlugin extends ConverterComponent {
    * Triggered when the converter begins converting a project.
    */
   private onBegin(context: Context) {
-    for (const key in context.project.reflections) {
-      if (context.project.reflections.hasOwnProperty(key)) {
-        const reflection = context.project.reflections[key];
-        const comment = reflection.comment;
-        // const mermaidCommentTag = comment.getTag('mermaid');
-        if (comment !== undefined) {
-          const mermaidTag = comment.getTag('mermaid');
-          if (mermaidTag !== undefined) {
-            const texts = mermaidTag.text.split('\n');
-            const title = texts.shift();
-            const mermaid = texts.join('\n');
-            mermaidTag.text = `#### ${title} \n\n <div class="mermaid">${mermaid}</div>`;
-          }
-        }
-      }
-    }
+    MermaidPlugin
+      .mermaidTags(context)
+      .forEach(MermaidPlugin.convertCommentTag);
   }
 
   /**
@@ -62,14 +132,9 @@ export class MermaidPlugin extends ConverterComponent {
    * Remove duplicate lines to tidy up output
    */
   private onPageEnd(page: PageEvent) {
-    const contents = page.contents;
-    if (contents !== undefined) {
-      if (MermaidPlugin.metaMatcher.test(contents)) {
-        page.contents = contents.replace(
-          MermaidPlugin.metaMatcher,
-          MermaidPlugin.script,
-        );
-      }
+    if (page.contents !== undefined) {
+      // convert
+      page.contents = MermaidPlugin.convertPageContents(page.contents);
     }
   }
 }

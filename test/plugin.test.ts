5034f39a4848fa29afe6dbb4882cbd128356efa8
kamiazya <yuki@kamiazya.tech>
2019-03-17 18:52:10 +0900

    add option to change mermaid.js version #9

--- test/plugin.test.ts
+++ test/plugin.test.ts
@@ -1,16 +1,42 @@
+import { Application } from 'typedoc/dist/lib/application';
 import { MermaidPlugin } from '../src/plugin';
 
 describe('MermaidPlugin', () => {
+  let plugin: MermaidPlugin;
+
+  beforeEach(() => {
+    const app = new Application();
+    plugin = new MermaidPlugin(app.converter);
+  });
+
   it('convert CommentTag', () => {
     const input = 'title\ngraph';
-    const result = MermaidPlugin.convertCommentTagText(input);
+    const result = plugin.convertCommentTagText(input);
     expect(result).toMatch('#### title');
     expect(result).toMatch('<div class="mermaid">graph<\/div>');
   });
 
   it('convert PageContents returns script tag for mermaid.js, initialize mermaid script, and body closing tag', () => {
     const input = '</body>';
-    const result = MermaidPlugin.convertPageContents(input);
+    const result = plugin.convertPageContents(input);
+    expect(result).toMatch('</body>');
+    expect(result).toMatch('mermaid.initialize({');
+    expect(result).toMatch(/src="https:\/\/unpkg.com\/mermaid@[\d|.]+\/dist\/mermaid.min.js"/);
+  });
+
+  it('can be change mermaid.js version when set \'mermaidVersion\' option', () => {
+    plugin.application.options.setValue('mermaidVersion', '0.0.0');
+
+    const input = '</body>';
+    const result = plugin.convertPageContents(input);
+    expect(result).toMatch('</body>');
+    expect(result).toMatch('mermaid.initialize({');
+    expect(result).toMatch(/src="https:\/\/unpkg.com\/mermaid@0\.0\.0\/dist\/mermaid.min.js"/);
+  });
+
+  it('load mermaid.js@7.1.2 when not to set \'mermaidVersion\' option', () => {
+    const input = '</body>';
+    const result = plugin.convertPageContents(input);
     expect(result).toMatch('</body>');
     expect(result).toMatch('mermaid.initialize({');
     expect(result).toMatch('src="https://unpkg.com/mermaid@7.1.2/dist/mermaid.min.js"');
@@ -18,7 +44,7 @@ describe('MermaidPlugin', () => {
 
   it('convert PageContents returns same value if body closing tag not exixt', () => {
     const input = 'hoge';
-    const result = MermaidPlugin.convertPageContents(input);
+    const result = plugin.convertPageContents(input);
     expect(result).toEqual('hoge');
   });
 });

54fb951a95571b18c9d3d661c58825560afb10b2
RunDevelopment <mitchi5000.ms@googlemail.com>
2021-10-30 17:02:29 +0200

    Added support for dark mode

--- test/plugin.test.ts
+++ test/plugin.test.ts
@@ -7,12 +7,12 @@ describe('MermaidPlugin', () => {
     const input = 'title\ngraph';
     const result = plugin.handleMermaidTag(input);
     expect(result).toMatch('#### title');
-    expect(result).toMatch('<div class="mermaid">graph</div>');
+    expect(result).toMatch('class="mermaid');
     expect(result).toMatchSnapshot();
   });
 
   it('convert PageContents returns script tag for mermaid.js, initialize mermaid script, and body closing tag', () => {
-    const input = '<div class="mermaid"></div></body>';
+    const input = '<head></head><body><div class="mermaid-block"></div></body>';
     const result = plugin.insertMermaidScript(input);
     expect(result).toMatch('</body>');
     expect(result).toMatch('mermaid.initialize({');
@@ -31,7 +31,7 @@ describe('MermaidPlugin', () => {
     const input = '#### title\n\n```mermaid\ngraph LR\n  <a> --> <b>\n```\n\nmore text';
     const result = plugin.handleMermaidCodeBlocks(input);
     expect(result).toMatch('#### title\n');
-    expect(result).toMatch('<div class="mermaid">graph LR\n  &lt;a&gt; --&gt; &lt;b&gt;</div>');
+    expect(result).toMatch('graph LR\n  &lt;a&gt; --&gt; &lt;b&gt;');
     expect(result).toMatch('\nmore text');
     expect(result).toMatchSnapshot();
   });
